05b157b2f714847a23fd63911e114b436fe2189a
kwakseungmin23 <lookingforfandl@gmail.com>
2023-02-11 10:19:10 +0900

    login practicing

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 const goodsRouter = require("./routes/goods");
 const cartsRouter = require("./routes/carts.js");
+const usersRouter = require("./routes/users");
 
 const connect = require("./schemas");
 connect();
 
-
 app.use(express.json());
-app.use("/api", [goodsRouter, cartsRouter]);
+app.use(express.urlencoded({ extended: false })); // form data 를 받기 위한 부분
+app.use(express.static("assets")); // front end file asset 확인
+app.use("/api", [goodsRouter, cartsRouter, usersRouter]);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(port, '포트로 서버가 열렸어요!');
+  console.log(port, "포트로 서버가 열렸어요!");
 });
---
+++ routes/users.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const userSchema = require("../schemas/user");
+
+router.post("/users", async (req, res) => {
+  const { email, nickname, password, confirmPassword } = req.body;
+  if (password !== confirmPassword) {
+    res.status(400).json({ err: "Password does not match." });
+    return;
+  } //password confirm logic
+
+  //email, nickname DB validation
+  const isExistUser = await userSchema.findOne({
+    $or: [{ email }, { nickname }], // email or nickname is true -> true
+  });
+  if (isExistUser) {
+    if (isExistUser.email === email) {
+      return res.status(400).send({ err: "password does not exist" });
+    } else if (isExistUser.nickname === nickname) {
+      return res.status(400).send({ err: "email does not exist" });
+    }
+  } else if (!isExistUser) {
+    const user = new userSchema({ email, nickname, password });
+    await user.save(); //DB SAVE
+    return res.status(201).json({}); // 201 -> something created
+  }
+});
+
+module.exports = router;
