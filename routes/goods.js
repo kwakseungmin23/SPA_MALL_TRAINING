4699e6da206750ddf469bced0c549340eb7b3076
kwakseungmin23 <lookingforfandl@gmail.com>
2023-02-11 14:39:21 +0900

    ITEMS GETTING API, SPECIFIC ITEM GETTING API

--- routes/goods.js
+++ routes/goods.js
@@ -1,51 +1,41 @@
 const express = require("express");
 const router = express.Router();
-
-const goods = [
-  {
-    goodsId: 4,
-    name: "상품 4",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
-    category: "drink",
-    price: 0.1,
-  },
-  {
-    goodsId: 3,
-    name: "상품 3",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
-    category: "drink",
-    price: 2.2,
-  },
-  {
-    goodsId: 2,
-    name: "상품 2",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
-    category: "drink",
-    price: 0.11,
-  },
-  {
-    goodsId: 1,
-    name: "상품 1",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
-    category: "drink",
-    price: 6.2,
-  },
-];
+const Goods = require("../schemas/goods");
 
 //상품 목록 조회 API
-router.get("/goods", (req, res) => {
-  res.json({ goods: goods });
+router.get("/goods", async (req, res) => {
+  const { category } = req.query;
+  const goods = await Goods.find(category ? { category } : {})
+    .sort("-date")
+    .exec();
+
+  const results = goods.map((item) => {
+    return {
+      goodsId: item.goodsId,
+      name: item.name,
+      price: item.price,
+      thumbnailUrl: item.thumbnailUrl,
+      category: item.category,
+    };
+  });
+  res.status(200).json({ goods: results });
 });
 
 //상품 상세 조회 API
-router.get("/goods/:goodsId", (req, res) => {
+router.get("/goods/:goodsId", async (req, res) => {
+  const { category } = req.query;
   const { goodsId } = req.params;
-  const [detail] = goods.filter((goods) => goods.goodsId === Number(goodsId));
-  res.json({ detail });
+  const goods = await Goods.findOne({ goodsId: goodsId }).sort("-date").exec();
+
+  const result = {
+    goodsId: goods.goodsId,
+    name: goods.name,
+    price: goods.price,
+    thumbnailUrl: goods.thumbnailUrl,
+    category: goods.category,
+  };
+
+  res.status(200).json({ goods: result });
 });
 
 const Cart = require("../schemas/cart.js");
@@ -59,13 +49,13 @@ router.post("/goods/:goodsId/cart", async (req, res) => {
     return res.status(400).json({
       success: false,
       errorMessage: "이미 장바구니에 해당하는 상품이 존재합니다.",
-    })
+    });
   }
 
   await Cart.create({ goodsId, quantity });
 
   res.json({ result: "success" });
-})
+});
 
 router.put("/goods/:goodsId/cart", async (req, res) => {
   const { goodsId } = req.params;
@@ -76,10 +66,10 @@ router.put("/goods/:goodsId/cart", async (req, res) => {
     await Cart.updateOne(
       { goodsId: goodsId },
       { $set: { quantity: quantity } }
-    )
+    );
   }
   res.status(200).json({ success: true });
-})
+});
 
 router.delete("/goods/:goodsId/cart", async (req, res) => {
   const { goodsId } = req.params;
@@ -90,9 +80,8 @@ router.delete("/goods/:goodsId/cart", async (req, res) => {
   }
 
   res.json({ result: "success" });
-})
+});
 
-const Goods = require("../schemas/goods.js");
 router.post("/goods", async (req, res) => {
   const { goodsId, name, thumbnailUrl, category, price } = req.body;
 
@@ -101,13 +90,18 @@ router.post("/goods", async (req, res) => {
   if (goods.length) {
     return res.status(400).json({
       success: false,
-      errorMessage: "이미 존재하는 GoodsId입니다."
+      errorMessage: "이미 존재하는 GoodsId입니다.",
     });
   }
 
-  const createdGoods = await Goods.create({ goodsId, name, thumbnailUrl, category, price });
-
+  const createdGoods = await Goods.create({
+    goodsId,
+    name,
+    thumbnailUrl,
+    category,
+    price,
+  });
   res.json({ goods: createdGoods });
-})
+});
 
 module.exports = router;
